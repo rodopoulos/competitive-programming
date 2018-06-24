9bf25f9c2e19f2a8417773a7a563413e310c7c5e
Felipe Rodopoulos <felipekss@gmail.com>
2018-06-23 22:50:39 -0300

    +1 URI problem 1288

---
+++ uri/1288.canhao-da-destruicao.cpp
@@ -0,0 +1,52 @@
+/** SOLUTION OVERVIEW
+ * This is exactly the knapsack problem.
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+int dp[51][100],
+    d[51], w[51];
+
+int max_damage(int n, int k) {
+  if (n == 0 || k == 0)
+    return 0;
+
+  if (w[n] > k)
+    return max_damage(n-1, k);
+
+  if (!dp[n][k]) {
+    dp[n][k] = max(
+      d[n] + max_damage(n-1, k - w[n]), // use the bullet
+      max_damage(n-1, k) // leave the bullet
+    );
+  }
+
+  return dp[n][k];
+}
+
+int main() {
+  int t;
+  cin >> t;
+
+  while (t--) {
+    int n, k, r, D;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+      cin >> d[i] >> w[i];
+    }
+
+    cin >> k >> r;
+
+    D = max_damage(n, k);
+
+    if (D < r) {
+      cout << "Falha na missao" << endl;
+    } else {
+      cout << "Missao completada com sucesso" << endl;
+    }
+    memset(dp,0,sizeof(dp));
+  }
+
+  return 0;
+}
